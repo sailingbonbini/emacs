08cb140b79c0d1ef7344067f5dabe57fa2ce4c48
Dohler <dohlertd@3c22fb003317.ant.amazon.com>
2021-09-24 14:58:05 +0200

    added a few things

--- init.el
+++ init.el
@@ -1,15 +1,58 @@
-;; Minimal UI
 (scroll-bar-mode -1)
+;; Minimal UI
 (tool-bar-mode   -1)
 (tooltip-mode    -1)
 (menu-bar-mode   -1)
 (blink-cursor-mode -1)
+(setq ring-bell-function 'ignore)
+(show-paren-mode)
+
+
+;; use fn key as control key
+(setq mac-function-modifier 'control)
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match
+   that used by the user's shell.
+
+   This is particularly useful under Mac OS X and macOS, where GUI
+   apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string
+			  "[ \t\n]*$" "" (shell-command-to-string
+					  "$SHELL --login -c 'echo $PATH'"
+						    ))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(set-exec-path-from-shell-PATH)
 
 (defun my/reload-config()
   (interactive)
   (load-file "~/.emacs.d/init.el"))
+
+(defun my/set-olivetti-org-faces()
+  (interactive)
+  (setq olivetti-body-width 100)
+  (custom-set-faces '(org-level-1 ((t (:inherit outline-1 :height 1.2)))))
+  (custom-set-faces '(org-level-2 ((t (:inherit outline-2 :height 1.1)))))
+  (custom-set-faces '(org-level-3 ((t (:inherit outline-3 :height 1.0)))))
+  (custom-set-faces '(org-level-4 ((t (:inherit outline-4 :height 1.0)))))
+  (custom-set-faces '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
+  (setq line-spacing 0.8)
+  (text-scale-set 2)
+  (centered-cursor-mode))
+
+(defun my/duplicate-line()
+  (interactive)
+  (move-beginning-of-line 1)
+  (kill-line)
+  (yank)
+  (open-line 1)
+  (next-line 1)
+  (yank))
 
 ;; Package configs
 (require 'package)
@@ -25,17 +68,48 @@
   (package-install 'use-package))
 (require 'use-package)
 
-;; Vim mode
-(use-package evil
-  :ensure t
+
+
+
+(use-package modus-themes
+  :ensure
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil
+        modus-themes-region '(bg-only no-extend))
+
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
   :config
-  (evil-mode 1))
+  ;; Load the theme of your choice:
+  (modus-themes-load-operandi) ;; OR (modus-themes-load-vivendi)
+  :bind ("<f5>" . modus-themes-toggle))
+
+
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   :config
+;;   (load-theme 'zenburn t)
+;; (zenburn-with-color-variables
+;;   (custom-theme-set-faces
+;;    'zenburn
+;;    `(org-level-1 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-2 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-3 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-4 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-5 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-6 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-7 ((t (:foreground ,zenburn-fg))))
+;;    `(org-level-8 ((t (:foreground ,zenburn-fg))))))
+;;   )
 
 ;; doom theme
-(use-package doom-themes
-  :ensure t
-  :config
-  (load-theme 'doom-one t))
+;;(use-package material-themes
+;;  :ensure t
+;;  :config
+;;  (load-theme 'material t))
+
 
 ;; ivy completion/switcher etc
 (use-package ivy
@@ -79,71 +153,132 @@
   :defer t)
 
 (use-package general
+  :ensure t)
+
+;; enable flyspell for all modes that derive from text mode
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+
+(setq ispell-program-name "/usr/local/bin/ispell")
+
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
+     (define-key flyspell-mouse-map [mouse-3] #'undefined)))
+
+;; distraction free writing mode
+(use-package olivetti
   :ensure t
-  :config (general-define-key
-  :states '(normal visual insert emacs)
-  :prefix "SPC"
-  :non-normal-prefix "M-SPC"
-  "/" '(swiper :which-key "Search for string")
-  "TAB" '(switch-to-other-buffer :which-key "other buffer")
-  "SPC" '(counsel-M-x :which-key "M-x")
-  ">" '(comment-line :which-key "comment line")
-  
-  ;; application settings
-  "a"  '(:ignore t :which-key "applications")
-  "at" '(ansi-term :which-key "terminal")
-  "ad" '(dired :which-key "dired")
-  
-  ;; buffer bindings
-  "b"  '(:ignore t :which-key "buffer")
-  "bb" '(ivy-switch-buffer :which-key "switch buffer")
-  "be" '(eval-buffer :which-key "eval buffer")
-  "bd" '(kill-buffer :which-key "delete buf")
+  :config
+  (add-hook 'olivetti-mode-hook 'my/set-olivetti-org-faces))
 
-  ;; emacs tools
-  "e" '(:ignore t :which-key "emacs")
-  "er"   '(my/reload-config :which-key "reload emacs config")
-  
-  ;;file bindings
-  "f"  '(:ignore t :which-key "file")
-  "ff" '(counsel-find-file :which-key "find file")
-  "fr" '(counsel-recentf :which-key "recent file")
-  "fp" '(td-load-profile :which-key "find bash profile")
+;;typewriter kind of scrolling
+(use-package centered-cursor-mode
+  :ensure t)
 
-  ;;help bindings
-  "h"  '(:ignore t :which-key "help")
-  "hf" '(counsel-describe-function :which-key "describe function")
-  "hv" '(counsel-describe-variable :which-key "describe variable")
-  "hi" '(counsel-info-lookup-symbol :which-key "lookup info for symbol")
-  "hu" '(counsel-unicode-char :which-key "unicode character")
+;;org bullets, make orgs headers look nicer
+(use-package org-superstar
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
 
-  ;;ui bindings
-  "u"  '(:ignore t :which-key "UI settings")
-  "ut" '(counsel-load-theme :which-key "Load theme")
-  "ul" '(global-linum-mode :which-key "Line numbers")
+;; allows to display word counts on subtrees etc
+(use-package org-wc
+  :ensure t)
 
-  ;; Window
-  "w"  '(:ignore t :which-key "window")
-  "wr"  '(windmove-right :which-key "move right")
-  "ws"  '(ivy-push-view :which-key "safe view")
-  "wd"  '(ivy-pop-view :which-key "delete view")
-  "wl"  '(windmove-left :which-key "move left")
-  "wu"  '(windmove-up :which-key "move up")
-  "wb"  '(windmove-down :which-key "move bottom")
-  "w/"  '(split-window-right :which-key "split right")
-  "w-"  '(split-window-below :which-key "split bottom")
-  "wd"  '(delete-window :which-key "delete window")
-  "wm"  '(delete-other-windows :which-key "maximize window")
-))
+(use-package cider
+  :ensure t)
+
+;; org mode setup
+(setq org-agenda-files '("~/org/inbox.org"
+			 "~/org/notes.org"
+			 "~/org/people.org"
+			 "~/org/meetings.org"))
+
+;; tags
+(setq org-tag-alist '(("sharon")
+		      ("markus") ("asim") ("tsufit") ("venkat")
+		      ("sharon") ("eyal") ("eli") ("doron")
+		      ("mariusz") ("samira") ("shimon") ("graeme") ("limor")
+		      ))
+
+
+;; capture templates
+(setq org-capture-templates '(( "t" "Task" entry
+				(file+headline "~/org/inbox.org" "Inbox")
+				"* TODO %?")
+			      ( "n" "Note" entry
+				(file+headline "~/org/inbox.org" "Inbox")
+				"* %?")
+			      ( "m" "Meeting" entry
+				(file+headline "~/org/inbox.org" "Inbox")
+				"* %U %?")))
+;; capture targets
+(setq org-refile-targets '((nil :maxlevel . 9)
+                                (org-agenda-files :maxlevel . 9)))
+(setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
+(setq org-refile-use-outline-path t)                  ; Show full paths for refiling
+(setq org-startup-indented 1)
+
+;; hide org's emphasis markers
+(setq org-hide-emphasis-markers t)
+
+
+
+(defun td/capture-task ()
+  (interactive)
+  (org-capture nil "t"))
+
+(general-define-key
+ "C-c c" 'counsel-org-capture
+ "C-x C-d" 'my/duplicate-line
+ "C-c r" 'councel-recentf
+ "C-c e" 'eval-buffer
+ )
+
+
+
+
+(general-define-key
+ :keymaps 'org-mode-map
+ "C-c a" 'org-agenda
+ "C-c C-a" 'org-archive-subtree
+ "C-c c" 'counsel-org-capture
+ "C-c d" 'org-todo
+ "C-c C-r" 'org-refile
+ "C-c C-t" 'counsel-org-tag
+ "C-c t" 'org-todo
+ "C-x C-r" 'counsel-recentf
+ "<f1>" 'td/capture-task) 
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "FOLLOW-UP(f@/!)" "WAITING(w@)" "|" "DONE(d!)" "CANCELLED(c@)")))
+
+(setq org-log-into-drawer t)
+
+
+(defun td/split-window-2-and-switch ()
+  (interactive)
+  (if (winum-get-window-by-number 2)
+      (winum-select-window-2)
+    (progn (split-window-horizontally)
+     (winum-select-window-2))))
+
+(defun td/split-window-3-and-switch ()
+  (interactive)
+  (if (winum-get-window-by-number 3)
+      (winum-select-window-3)
+    (progn (split-window-vertically)
+     (winum-select-window-3))))
+
 
 (general-define-key
  "s-1" 'winum-select-window-1
- "s-2" 'winum-select-window-2
- "s-3" 'winum-select-window-3
+ "s-2" 'td/split-window-2-and-switch
+ "s-3" 'td/split-window-3-and-switch
  "s-4" 'winum-select-window-4
  "s-5" 'winum-select-window-5)
 
-(defun td-load-profile ()
+(defun td/load-profile ()
   (interactive)
   (find-file "~/.bash_profile"))
 
@@ -163,15 +298,24 @@
 			  "Env"
 			  (("a" pyenv-workon "activte")
 			   ("d" pyenv-deactivate "deactivate"))))
+
+(major-mode-hydra-define org-mode nil
+  ("Insert"
+   (("l" org-insert-link "link")
+    ("t" counsel-org-tag "tag"))
+   "Tools"
+   (("w" count-words "count words"))))
+
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ace-window-display-mode t)
  '(package-selected-packages
    (quote
-    (winum flycheck which-key use-package nlinum major-mode-hydra general evil elpy doom-themes counsel ace-window))))
+    (modus-themes cider zenburn-theme winum which-key use-package org-wc org-superstar olivetti nlinum major-mode-hydra general flycheck elpy doom-themes counsel centered-cursor-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
